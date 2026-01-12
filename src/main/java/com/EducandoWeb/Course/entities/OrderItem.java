8db75f98af4a7c1fcd1417fcb08086384ac6eefd
EmannuelBezerra <emannuelbezerra678@gmail.com>
2026-01-12 18:33:22 -0300

    Métodos Subtotal e total

--- src/main/java/com/EducandoWeb/Course/entities/OrderItem.java
+++ src/main/java/com/EducandoWeb/Course/entities/OrderItem.java
@@ -1,6 +1,7 @@
 package com.EducandoWeb.Course.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import com.EducandoWeb.Course.entities.pk.OrderItemPK;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -64,6 +65,29 @@ public class OrderItem implements Serializable{
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 	
 	
 }
