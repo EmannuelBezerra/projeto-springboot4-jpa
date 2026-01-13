02dd17b76abf2cd9bc3041d6138dcdd02703500a
EmannuelBezerra <emannuelbezerra678@gmail.com>
2026-01-12 22:22:32 -0300

    Tratamento de execeção - delete

--- src/main/java/com/EducandoWeb/Course/services/UserService.java
+++ src/main/java/com/EducandoWeb/Course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.EducandoWeb.Course.entities.User;
 import com.EducandoWeb.Course.repositories.UserRepository;
+import com.EducandoWeb.Course.services.exceptions.DatabaseException;
 import com.EducandoWeb.Course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,8 +32,14 @@ public class UserService {
 		return repository.save(obj);
 	}
 	
-	public void delete(Long id) {
-		repository.deleteById(id);
+	public void delete(Long id) { 
+	    repository.findById(id).orElseThrow(
+	        () -> new ResourceNotFoundException(id)); 
+	    try { 
+	        repository.deleteById(id); 
+	    } catch (DataIntegrityViolationException e) { 
+	        throw new DatabaseException(e.getMessage()); 
+	    }  
 	}
 	
 	public User update(Long id, User obj) {
